059dc1d84465387b8c78dbe9987a2f7e1d29093e
AngelaTC <tc.angela.m@gmail.com>
2020-11-24 22:02:34 -0600

    Creación de carpeta de rutas

--- Server/server.js
+++ Server/server.js
@@ -10,103 +10,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json())
- 
-// GET:Obtener.
-// res: response/respuesta.-recibo.
-// req: request/pedir.-mando.
+
+// Saludo:
 app.get('/', function (req, res) {
   res.send('<h1>Quiuboles al servidor REST.</h1>');
 });
 
-app.get('/usuario', function (req, res) {
-    res.json({
-      // 200: Todo salió bien.
-      ok: 200,
-      mensaje: '<h1>Quiuboles al Usuarios.</h1>'});
-  });
-
-  app.get('/usuario', function (req, res) {
-    res.json({
-      // 200: Todo salió bien.
-      ok: 200,
-      mensaje: 'Jelou'});
-  });
-
-// app.get('/saludo', function (req, res) {
-//     res.json({
-//         ok: '200', 
-//         mensaje: 'Bienvenido'
-//     });
-//   });
+// Importación de rutas:
+app.use(require('./routes/usuario'));
  
-  // POST: recibir?
-  app.post('/saludo', function(req, res){
-    res.json({
-      ok: 200,
-      mensaje: 'Usuario insertado con éxito.'
-    })
-  });
-
-  app.post('/usuario', function(req,res){
-    let nombre = id.body.nombre;
-    let body = req.body;
-
-    if(nombre === undefined){
-      res.status(400).json({
-        ok: 400,
-        mensaje: 'Favor de mandar el valor del nombre'
-      })
-    }else{
-
-    
-    res.json({
-      ok: 200,
-      mensaje: 'Usuario insertado con éxito.',
-      nombre: nombre
-      
-    });
-    }
-  });
-
-  
-  // PUT:Actualizar.
-  // params: parámetros.
-  app.put('/usuario/:id/:nombre', function(req,res){
-    let id = req.params.id;
-    let nombre = req.params.nombre;
-    
-    res.json({
-      ok: 200,
-      mensaje: 'Usuario actualizado con éxito.',
-      id: id,
-      nombre: nombre
-    });
-  });
-  
-  app.put('/usuario', function(req,res){
-    let nombre = id.body.nombre;
-    let body = req.body;
-
-    if(nombre == undefined){
-
-    }
-    res.json({
-      ok: 200,
-      mensaje: 'Usuario insertado con éxito.',
-      nombre: nombre
-    })
-  });
-  
-  // DELETE:
-  app.delete('/usuario', function(req, res){
-    let id = req.params.id;
-
-    res.json({
-      ok: 200,
-      mensaje: 'Usuario eliminado con éxito.',
-      id: id
-    });
-  });
 
 // Conexión a la base de datos Mongoose:
  mongoose.connect('mongodb://localhost:27017/cafeteria', {
@@ -117,7 +29,7 @@ app.get('/usuario', function (req, res) {
  }, (err, res) => {
 
   if (err) throw new err;
-  console.log('Base de datos online :)');
+  console.log('Base de datos ONLINE c:');
 });
 
 // listen: escucha la app.
