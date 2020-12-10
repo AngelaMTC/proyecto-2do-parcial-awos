0554ad4ce375ccda01ee2201affc41528c955d03
AngelaTC <tc.angela.m@gmail.com>
2020-12-09 18:23:25 -0600

    Habilitación de CORS.(DAPS).

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,20 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
  
+// Habilitar CORS:
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
+});
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
